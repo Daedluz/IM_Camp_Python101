346b45eeb38f3c2bde8d3546d76a106135a8b60a
Daedluz <hiiammichael0520@gmail.com>
2022-01-21 03:27:37 +0800

    Don't mess up tmr

---
+++ test.py
@@ -0,0 +1,14 @@
+def greetings(name):
+    print(f'{name}, Happy New Year !')
+greetings("Cherry")
+
+thisDict = {
+    "First_name": "Michael",
+    "Last_name": "Chen",
+    "School": "NTU",
+    "Department": "IM",
+    "Job": "Clown",
+    "Birthday": "2001-4-19"
+}
+
+print (thisDict["First_name"])
